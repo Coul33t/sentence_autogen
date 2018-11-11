94c4d59273115fc671204135fdae38c69f58d209
Coul33t <Coulis1990@gmail.com>
2018-11-11 11:44:31 +0100

    Minor

--- main.py
+++ main.py
@@ -6,56 +6,66 @@ import argparse
 
 import pdb
 
+
 def parse_args():
     """ Arguments parser. """
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", help="the file containing the sentences")
     args = parser.parse_args()
-    
+
     if not args.file:
         parser.error("Error: file required as an argument (-f or --file).")
 
     return args
+
 
 def purify(sentences):
     """ Gets rid of all punctuation signs. """
 
     # maketrans('this_is_mapped_to...', '...this', 'erase this')
     translator = str.maketrans('', '', string.punctuation)
-    if type(sentences) == str:
+    if isinstance(sentences, str):
         return sentences.translate(translator)
 
-    elif type(sentences) == list:
+    elif isinstance(sentences, list):
         new_lst = []
         for sentence in sentences:
             new_lst.append(sentence.translate(translator))
         return new_lst
 
-# 
+    return sentences
+
+
 def beautify(sentences):
-    """ Reformats sentences by adding a space before and after punctuation (to treat them as regular sentence parts). """
+    """ Reformats sentences by adding a space before and after punctuation
+        (to treat them as regular sentence parts). """
 
     translator = str.maketrans({key: " {0} ".format(key) for key in ',\''})
 
-    if type(sentences) == str:
-        return re.sub( r'\s+', ' ', sentences.translate(translator)).strip()
+    if isinstance(sentences, str):
+        return re.sub(r'\s+', ' ', sentences.translate(translator)).strip()
 
-    elif type(sentences) == list:
+    elif isinstance(sentences, list):
         new_lst = []
         for sentence in sentences:
-            new_lst.append(re.sub( r'\s+', ' ', sentence.translate(translator)).strip())
+            new_lst.append(re.sub(r'\s+', ' ', sentence.translate(translator)).strip())
         return new_lst
+
+    return sentences
 
 
 def compute_words(sentences):
     """ Returns a set of unique words across all the sentences. """
 
-    yo = set()
+    words_set = set()
+
     for sentence in sentences:
         words = sentence.split(' ')
         for word in words:
-            yo.add(word)
-    return yo
+            words_set.add(word)
+
+    return words_set
 
 
 def compute_word_occurence(sentences, word_order):
@@ -68,7 +78,7 @@ def compute_word_occurence(sentences, word_order):
 
     for sentence in sentences:
         splitted = sentence.split(' ')
-        for i,word in enumerate(splitted):
+        for i, word in enumerate(splitted):
             if i == 0:
                 if 'BEGIN' in matrix[word]:
                     matrix[word]['BEGIN'] += 1
@@ -89,25 +99,27 @@ def compute_word_occurence(sentences, word_order):
 
 def normalise_word_matrix(word_matrix):
     """ Normalises the number of occurences of each word (from occurences to probabilities). """
+
     new_matrix = copy.deepcopy(word_matrix)
 
-    for k, v in new_matrix.items():
-        total_sum = sum(v.values())
+    for word, probs in new_matrix.items():
+        total_sum = sum(probs.values())
 
-        for kk in v.keys():
-            new_matrix[k][kk] = new_matrix[k][kk] / total_sum
+        for next_word_prob in probs.keys():
+            new_matrix[word][next_word_prob] = new_matrix[word][next_word_prob] / total_sum
 
     return new_matrix
 
 def cumulative_probs(lst):
     """ Orders the a list of probabilities and transforms them into cumulative probabilities. """
+
     total_sum = sum([x[1] for x in lst])
 
     # Normalising (-> probs)
     lst = [[x[0], x[1] / total_sum] for x in lst]
 
     # Ordering probs
-    lst = sorted(lst ,key=lambda x: x[1], reverse=False)
+    lst = sorted(lst, key=lambda x: x[1], reverse=False)
 
     # Cumulative probs
     for i, _ in enumerate(lst):
@@ -118,30 +130,33 @@ def cumulative_probs(lst):
 
 def return_selected(random_choice, lst):
     """ Chose a random word from a probabilities list. """
+
     for i, word in enumerate(lst):
         if i == 0:
             if random_choice < word[1]:
                 return word[0]
-                break
 
         if i == len(lst) - 1:
             return word[0]
-            break
 
         else:
             if lst[i-1][1] <= random_choice < lst[i][1]:
                 return word[0]
-                break
+
+    # Shouldn't happens but pylint is bothering me with
+    # it.
+    return lst[0][0]
 
 def generate_sentence(wpm, wpm_normalised):
     """ Generates a sentence from a list of word probabilities. """
+
     new_sentence = ''
     first_word = []
     last_word = ''
 
-    for k, v in wpm.items():
-        if 'BEGIN' in v:
-            first_word.append([k, wpm[k]['BEGIN']])
+    for word, next_word_proba in wpm.items():
+        if 'BEGIN' in next_word_proba:
+            first_word.append([word, wpm[word]['BEGIN']])
 
     first_word = cumulative_probs(first_word)
 
@@ -152,9 +167,10 @@ def generate_sentence(wpm, wpm_normalised):
     new_sentence += first_choice + ' '
     last_word = first_choice
 
-    for i in range(30):
+    # TODO: if X words is reached, go for the next with END
+    for _ in range(30):
         next_word_proba = wpm_normalised[last_word]
-        next_word_proba_lst = [[k, v] for k,v in next_word_proba.items() if k != 'BEGIN']
+        next_word_proba_lst = [[k, v] for k, v in next_word_proba.items() if k != 'BEGIN']
         next_word_proba_lst = cumulative_probs(next_word_proba_lst)
         random_choice = rn.random()
         choice = return_selected(random_choice, next_word_proba_lst)
@@ -169,6 +185,7 @@ def generate_sentence(wpm, wpm_normalised):
 
 def reformate_sentence(sentence):
     """ Reformat the sentence correctly, such as punctuation have the correct spacing. """
+
     sentence = sentence.replace(' , ', ', ')
     sentence = sentence.replace(' \' ', '\'')
     return sentence
@@ -181,8 +198,6 @@ def main():
     # Load the sentences
     with open(args.file, 'r', encoding='utf-8') as input_file:
         initial_sentences = input_file.read().split('\n')
-
-    word_proba = []
 
     # Add spaces before and after quotes and commas
     trimmed = beautify(initial_sentences)
@@ -197,7 +212,7 @@ def main():
     word_prob_matrix_normalised = normalise_word_matrix(word_prob_matrix)
 
     # Generate sentences
-    for i in range(10):
+    for _ in range(10):
         print(reformate_sentence(generate_sentence(word_prob_matrix, word_prob_matrix_normalised)))
         print('\n')
 
