962490d08f32fe3fe7f007e078fa19d3c2032125
Quentin <Coulis1990@gmail.com>
2018-11-21 16:03:48 +0100

    Now using a Markold object instead of multiple functions

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
 
-from markold import multi_markov
-from markold.tools import reformate_sentence
+from markold.markold import Markold
 import pdb
 
 
@@ -27,32 +26,15 @@ def parse_args():
 
 
 def main_multi(args):
-    initial_sentences = []
-
     file = args.file
     markov = int(args.markov)
     number = int(args.number)
     output = args.output
 
-    # Load the sentences
-    with open(file, 'r', encoding='utf-8') as input_file:
-        initial_sentences = input_file.read().split('\n')
+    markold = Markold()
+    markold.import_sentences(file)
+    markold.generate_multiple_sentences(markov, number, to_output=output, to_print=args.print)
 
-    # Generate sentences
-    generated_sentences = multi_markov.examples_to_sentences(initial_sentences, markov, number_to_generate=number, min_word_length=15, max_word_length=150)
-
-    # Print and/or write them
-    if output:
-        output_file = open(output, 'a', encoding='utf-8')
-
-    for sentence in generated_sentences:
-        if args.print:
-            print(reformate_sentence(sentence))
-        if output:
-            output_file.write(reformate_sentence(sentence) + '\n')
-
-    if output:
-        output_file.close()
 
 if __name__ == '__main__':
     args = parse_args()
